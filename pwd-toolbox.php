ffce4cd722839846283711e3151306a8824ed67f
Intriligator <daniel@peekskillwebdesign.com>
2016-04-29 09:50:22 -0400

    social widget now accepts web addresses with https, http  and without any protocol

--- pwd-toolbox.php
+++ pwd-toolbox.php
@@ -329,9 +329,12 @@ class Social_Widget extends WP_Widget {
    */
  
   public function widget( $args, $instance ) {
+    if(strpos($instance['social_site'], 'https://') == false && strpos($instance['social_site'], 'http://') == false){
+      $instance['social_site'] = 'http://' . $instance['social_site'];
+    }
     echo $args['before_widget'];
     if ( ! empty( $instance['social_site'] ) ) {
-      echo $args['before_widget'] ?><a href="http://<?php echo $instance['social_site'] ?>" target="_blank"><i class="fa fa-<?php echo $instance['font_awesome'] ?>"></i></a><?php $args['after_widget'];
+      echo $args['before_widget'] ?><a href="<?php echo $instance['social_site'] ?>" target="_blank"><i class="fa fa-<?php echo $instance['font_awesome'] ?>"></i></a><?php $args['after_widget'];
     }
   }
 
@@ -353,30 +356,15 @@ class Social_Widget extends WP_Widget {
     <label for="<?php echo $this->get_field_id( 'font_awesome' ); ?>"><?php _e( 'Social Media Logo:' ); ?></label> 
     <select class="widefat" id="<?php echo $this->get_field_id( 'font_awesome' ); ?>" name="<?php echo $this->get_field_name( 'font_awesome' ); ?>"  value="<?php echo esc_attr( $font_awesome ); ?>">
     
-    <?php if ($font_awesome == 'instagram') : ?>
-      <option value="instagram" selected>Instagram</option>
-    <?php else : ?>
-      <option value="instagram">Instagram</option>
-    <?php endif; ?>
+    <?php pwd_social_widget_option($font_awesome, 'instagram', 'Instagram'); ?>
+    <?php pwd_social_widget_option($font_awesome, 'facebook', 'Facebook'); ?>
+    <?php pwd_social_widget_option($font_awesome, 'facebook-square', 'Facebook Square Logo'); ?>
+    <?php pwd_social_widget_option($font_awesome, 'pinterest', 'Pinterest'); ?>
+    <?php pwd_social_widget_option($font_awesome, 'pinterest-square', 'Pinterest Square Logo'); ?>
+    <?php pwd_social_widget_option($font_awesome, 'twitter', 'Twitter'); ?>
+    <?php pwd_social_widget_option($font_awesome, 'twitter-square', 'Twitter Square Logo'); ?>
+    <?php pwd_social_widget_option($font_awesome, 'youtube', 'YouTube'); ?>
           
-    <?php if ($font_awesome == 'facebook') : ?>
-      <option value="facebook" selected>Facebook</option>
-    <?php else : ?>
-      <option value="facebook">Facebook</option>
-    <?php endif; ?>
-
-    <?php if ($font_awesome == 'pinterest') : ?>
-      <option value="pinterest" selected>Pinterest</option>
-    <?php else : ?>
-      <option value="pinterest">Pinterest</option>
-    <?php endif; ?>
-
-    <?php if ($font_awesome == 'twitter') : ?>
-      <option value="twitter" selected>Twitter</option>
-    <?php else : ?>
-      <option value="twitter">Twitter</option>
-    <?php endif; ?>
-
     </select>
     </p>
     <?php 
@@ -389,16 +377,26 @@ class Social_Widget extends WP_Widget {
    * @param array $new_instance The new options
    * @param array $old_instance The previous options
    */
-  public function update( $new_instance, $old_instance ) {
+public function update( $new_instance, $old_instance ) {
     $instance = array();
     $instance['social_site'] = ( ! empty( $new_instance['social_site'] ) ) ? strip_tags( $new_instance['social_site'] ) : '';
     $instance['font_awesome'] = ( ! empty( $new_instance['font_awesome'] ) ) ? strip_tags( $new_instance['font_awesome'] ) : '';
     return $instance;
   }
 }
+
 function register_social_widget() {
     register_widget( 'social_widget' );
 }
 add_action( 'widgets_init', 'register_social_widget' );
+function pwd_social_widget_option($font_awesome, $input, $display_name){
+
+if ($font_awesome == $input) : ?>
+      <option value="<?php echo $input?>" selected><?php echo $display_name ?></p></option>
+    <?php else : ?>
+      <option value="<?php echo $input ?>"><?php echo $display_name ?></option>
+    <?php endif; 
+
+}
 // ********************** 10. END SOCIAL WIDGET ********************** //
 ?>
