e3bfdb6360fdf0ef78752b1398bb634995f9a846
Intriligator <daniel@peekskillwebdesign.com>
2016-05-26 14:14:36 -0400

    client can now read

--- modules/add-client-user.php
+++ modules/add-client-user.php
@@ -1,7 +1,8 @@
 <?php
 add_action('init', 'pwd_add_client');
 function pwd_add_client(){
-	add_role('client', __( 'Client' ), array('read => false'));
+	add_role('client', __( 'Client' ), array('read => true'));
+
 	if(current_user_can('client')){
 		show_admin_bar(false);
 	}
