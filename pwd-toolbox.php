ea99747659915f02a3886be74c72f71f33e195af
Intriligator <Daniel@peekskillwebdesign.com>
2016-06-08 14:55:46 -0400

    Update pwd-toolbox.php

--- pwd-toolbox.php
+++ pwd-toolbox.php
@@ -3,7 +3,7 @@
 Plugin Name: PWD Toolset
 Description: A toolset for websites developed by Peekskill Web Design
 Author:      Peekskill Web Design
-Version: 0.8.43
+Version: 0.8.44
 GitHub Plugin URI: https://github.com/PeekskillWebDesign/pwd-toolbox
 */
 
