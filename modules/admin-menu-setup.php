4fc98f3b22fcd547fc51292504e81a669e4d7577
Intriligator <daniel@peekskillwebdesign.com>
2016-06-14 13:07:12 -0400

    peekskill logo added

--- modules/admin-menu-setup.php
+++ modules/admin-menu-setup.php
@@ -9,6 +9,6 @@ add_action( 'admin_action_cpt_delete_button', 'cpt_delete_button_admin_action' )
 add_action( 'admin_action_pwd_maintenance', 'pwd_maintenance_admin_action' );
 
 function PWD_toolbox_menu() {
-add_menu_page( 'PWD Theme Options', 'PWD Theme Options', 'manage_options', 'pwdtoolbox', 'PWD_toolbox_options','dashicons-admin-tools');
+add_menu_page( 'PWD Theme Options', 'PWD Theme Options', 'manage_options', 'pwdtoolbox', 'PWD_toolbox_options',plugin_dir_url(dirname(__FILE__)) . '/images/PWD-logo.png');
 }
 ?>
