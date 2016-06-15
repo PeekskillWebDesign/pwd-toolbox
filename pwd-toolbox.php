a05d9500882bbc0c19db87a5be19afafcded0a22
Intriligator <daniel@peekskillwebdesign.com>
2016-06-15 13:17:49 -0400

    fixed shortcode menu problem

--- pwd-toolbox.php
+++ pwd-toolbox.php
@@ -3,7 +3,7 @@
 Plugin Name: PWD Toolset
 Description: A toolset for websites developed by Peekskill Web Design
 Author:      Peekskill Web Design
-Version: 0.8.54
+Version: 0.8.55
 GitHub Plugin URI: https://github.com/PeekskillWebDesign/pwd-toolbox
 */
 
